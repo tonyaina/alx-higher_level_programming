bf5620ec0a40b1bd582de659257cf24ccd930d6e
tonyaina <t.awolowo19911@gmail.com>
2023-01-11 02:26:53 -0800

    To print alphabets in python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """print the alphabets in lowercase, not followed by a new line"""
 
-for j in range(97, 123):
-    print(chr(j), end=' ')
+for letter in range(97, 123):
+    print("{}", format(chr(letter)), end="")
