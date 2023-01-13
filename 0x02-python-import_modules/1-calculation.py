4044ccb4bb31e1a16ee677dd8e099520a9eef287
tonyaina <t.awolowo19911@gmail.com>
2023-01-13 14:54:55 -0800

    file for calculations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     """print mathematical calculations of addition,
     substration, multiplication and division quotient of 10 and 5."""
 
-    from calculator_import add, sub, mul, div
+    from calculator import add, sub, mul, div
 
     a = 10
     b = 5
