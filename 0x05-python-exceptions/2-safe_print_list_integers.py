8aa9328d6c2277709b38401f098513e184378f4c
tonyaina <t.awolowo19911@gmail.com>
2023-01-24 09:55:56 -0800

    printed list of integers safe

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """print the first x element of a list that are integers.
+    Args:
+        my_list (list): The list to  print elements from.
+        x (int).
+    Return:
+          the No. of elements printed."""
+
+    ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
