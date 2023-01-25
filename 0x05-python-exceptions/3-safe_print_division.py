c607ba88a46aea771b8233c0124ade5ea680610e
tonyaina <t.awolowo19911@gmail.com>
2023-01-25 14:32:00 -0800

    how to safely print division N.

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """print return the division of a by b."""
+
+    try:
+        div = a/b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
