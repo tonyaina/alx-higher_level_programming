43a3777c4c349b310e0a047efc33223fce359acd
tonyaina <t.awolowo19911@gmail.com>
2023-01-25 14:18:51 -0800

    safe printed list of file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    """print x element of a list and my_list which contain (integer, string etc."""
+    """print x element of a list.
+    Agrs:
+        my_list (list): The list to be printed of elements.
+        x (int): No. of elements my_list to print.
+    Return:
+        No. of printed elements."""
 
     ret = 0
     for i in range(x):
