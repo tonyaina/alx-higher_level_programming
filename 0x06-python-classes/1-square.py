261cfa8227080678904101edc754d2ef7c0aac52
tonyaina <t.awolowo19911@gmail.com>
2023-01-29 06:28:45 -0800

    second printed square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+class Square:
+    """Represent a square."""
+
+    def_init_(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self._size = size
