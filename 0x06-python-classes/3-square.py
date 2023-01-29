a79dd30fa5d4e715b2db252b4c11fea2cf7abe4a
tonyaina <t.awolowo19911@gmail.com>
2023-01-29 07:45:29 -0800

    print area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,7 +2,8 @@
 """print Define a class Square."""
 
 class Square:
-    
+    """Represent a square."""
+
     def __init__(self, size=0):
         """Initialize a new square.
         Args:
@@ -14,5 +15,5 @@ class Square:
         self.__size = size
 
     def area(self):
-
+        """Return the current area of the square."""
         return (self.__size * self.__size)
