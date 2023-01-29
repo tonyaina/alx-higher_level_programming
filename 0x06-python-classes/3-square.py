e56859ac196075e1e623f1e405f13e1ccebaac1d
tonyaina <t.awolowo19911@gmail.com>
2023-01-29 07:42:16 -0800

    print area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,7 @@
 
 class Square:
     
-    def __init__(size, size=0):
+    def __init__(self, size=0):
         """Initialize a new square.
         Args:
             size (int): the size of the square."""
@@ -12,5 +12,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
+
         return (self.__size * self.__size)
