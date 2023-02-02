cd38f3e0af0fc37a9b56c6b09f5bd5761ffb0928
tonyaina <t.awolowo19911@gmail.com>
2023-02-02 05:29:54 -0800

    print area of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Define a class Rectangle."""
+
+class Rectangle:
+    """Represent a class rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new rectangle.
+        Args:
+            size (int): the size of a new rectangle."""
+
+        self.width = width
+        self.height = height
+        
+        @property
+
+        def width(self):
+            """Retrieved the current size of the rectangle."""
+            return (self.__width)
+
+        @width.setter
+
+        def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            elif value > 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+
+        def height(self):
+            """Retrieved the current size of the rectangle."""
+            return (self.__height)
+
+        @height.setter
+
+        def height(self, value):
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            elif value > 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+
+        def area(self):
+            """Return the current area of the rectangle."""
+            return (self.__width * self.__height)
+
