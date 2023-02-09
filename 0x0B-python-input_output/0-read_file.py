255b8d769e08f0b091a981483320412960e0b776
tonyaina <t.awolowo19911@gmail.com>
2023-02-09 02:08:06 -0800

    read file in python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
+
     with open(filename, "r") as f:
-        for 1i in f:
-            print(1i, end="")
+        for li in f:
+            print(li, end="")
     f.closed
